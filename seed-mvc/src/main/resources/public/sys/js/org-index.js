34ab88975af7c0f4f2202a48974eafd2eceeebaa
OP <panlc2004@126.com>
2017-08-20 00:31:25 +0800

    router加载方式实现

--- seed-mvc/src/main/resources/public/sys/js/org-index.js
+++ seed-mvc/src/main/resources/public/sys/js/org-index.js
@@ -1,22 +1,31 @@
-var orgIndex = Vue.extend({
+sys$org_index_html = Vue.extend({
     template: '#indexDiv',
     mixins: [czyPageBar],
-    data:function() {
-        return{
+    data: function () {
+        return {
             url: ctx + '/sys/org/selectPageByParams'
-
         }
     },
     methods: {
-        search:function () {
+        search: function () {
             alert(123);
         },
         toEdit: function (entity) {
-            czy.win.open('机构信息维护', ctx + '/sys/org-edit.html' ,["400px","450px"]);
+            czy.win.open('机构信息维护', ctx + '/sys/org-edit.html', ["400px", "450px"]);
             czy.param.temp.setEntity(entity);
         },
-        del:function () {
-            
+        del: function () {
+
         }
+    },
+    created: function () {
+        //创建定时器一定要在destroyed里销毁！
+        this.timer = window.setInterval(function () {
+            console.log(123123)
+        }, 1000)
+    },
+    destroyed: function () {
+        //创建定时器一定要在destroyed里销毁！
+        window.clearInterval(this.timer);
     }
-})
+});
--- seed-mvc/src/main/resources/static/common/js/index.js
+++ seed-mvc/src/main/resources/static/common/js/index.js
@@ -1,21 +1,8 @@
 /**
  * Created by PLC on 2017/8/6.
  */
-// iframe自适应
-// $(window).on('resize', function () {
-//     var $content = $('.content');
-//     console.log($content.width())
-//     // var remove = 120;
-//     // //设置页面加载区域高度
-//     // $content.height($(this).height() - remove);
-//     // //设置页面加载frame高度
-//     $content.find('iframe').each(function () {
-//         $(this).width($content.width());
-//     });
-// }).resize();
-
-// 主菜单栏
-var menuItem = Vue.extend({
+// 菜单栏
+Vue.component('menuItem',{
     name: 'menu-item',
     props: {item: {}},
     template: [
@@ -29,39 +16,109 @@ var menuItem = Vue.extend({
     ].join(''),
     methods: {
         loadInFrame: function (child) {
-            console.log(child)
-            window.location.hash = child.url
         },
         loadInTab:function (child) {
 
         },
         loadPage: function (child) {
-            if(window.pageloadInTab) {
-                this.loadInTab(child);
-            } else {
-                this.loadInFrame(child);
-            }
+            loadComponent(child.url);
+            // if(window.pageloadInTab) {
+            //     this.loadInTab(child);
+            // } else {
+            //     this.loadInFrame(child);
+            // }
         }
     }
 
 })
 
+// 定义路由
+var routes = [];
+var router = new VueRouter({
+    routes: routes
+});
 
-Vue.component('menuItem', menuItem);
+/**
+ * 加载组件并设置路由
+ * @param menu
+ */
+function loadComponent(url) {
+    $("#component-cache").load(ctx + url,function (data,status) {
+        if(status=='success') {
+            var component_name = buildComponentNameByUrl(url);
+            var router_path = '/' + component_name;
+            var component = eval(component_name)
+            router.addRoutes([{path: router_path, component: component}])
+            router.push(router_path);
+        } else {
+            alert('error');
+        }
+    });
+}
 
-window.mainPanel = new Vue({
+String.prototype.startWith=function(str){
+    var reg=new RegExp('^'+str);
+    return reg.test(this);
+}
+
+String.prototype.endWith=function(str){
+    var reg=new RegExp(str+"$");
+    return reg.test(this);
+}
+
+String.prototype.replaceAll=function(str,value){
+    var regExp = new RegExp(str,'gm');
+    return this.replace(regExp, value);
+}
+
+/**
+ * 计算组件名称
+ * @param url
+ */
+function buildComponentNameByUrl(url) {
+    if(!url.startWith('/')) {
+        alert("url必须以'/'开头")
+        return;
+    }
+    var component_name = url.replace('/','').replaceAll('\/', '$').replaceAll('-', '_');
+    if(url.endWith('.html')){
+        component_name = component_name.replace('.html', '_html');
+    }
+    return component_name
+}
+
+/**
+ * 根据window.location.hash计算应该加载的url
+ * @param locationHash
+ * @returns {*}
+ */
+function buildUrlByWindowLocationHash(locationHash) {
+    locationHash = locationHash.replace('#','')
+    if(!locationHash.startWith('/')) {
+        alert("url必须以'/'开头")
+        return;
+    }
+    if(locationHash.endWith('_html')) {
+        locationHash = locationHash.replace('_html','.html');
+    }
+    var url = locationHash.replaceAll('\\$', '/').replaceAll('_', '-');
+    return url;
+}
+
+seed = new Vue({
     el: '#main-panel',
+    router: router,
     data: {
         loadUrl: 'homePage.html',
         menuList: {},
         navTitle: '',
         collapse: false,
-        tempValue:{}
+        defaultActive:'1'
     },
     methods: {
         getMenu: function () {
             $.get("/sys/resource/findResourceTreeForLoginUser?_" + $.now(), function (o) {
-                mainPanel.menuList = o.data[0].children;
+                seed.menuList = o.data[0].children;
             });
         },
         openWin: function (title, url, area) {
@@ -71,32 +128,12 @@ window.mainPanel = new Vue({
     created: function () {
         this.getMenu();
     },
-    updated: function () {
-        //路由
-        var router = new Router();
-        routerList(router, mainPanel.menuList);
-        router.start();
+    mounted:function () {
+        //根据url加载对应页面
+        var url = buildUrlByWindowLocationHash(window.location.hash);
+        loadComponent(url);
+        this.defaultActive = '2';   //让指定菜单置为激活状态 TODO
     }
+
 });
 
-function routerList(router, menuList) {
-    for (var key in menuList) {
-        var menu = menuList[key];
-        if (menu.types == 2) {
-            routerList(router, menu.children);
-        } else if (menu.types == 1) {
-            router.add('#' + menu.url, function () {
-                var url = window.location.hash;
-                //替换iframe的url
-                mainPanel.loadUrl = url.replace('#', '') + ".html";
-                //导航菜单展开
-                $(".active").removeClass("active");
-                $("a[href='" + url + "']").parents("li").addClass("active");
-
-                mainPanel.navTitle = $("a[href='" + url + "']").text();
-                // 开启加载条
-                // mainPanel.$Loading.start();
-            });
-        }
-    }
-}
