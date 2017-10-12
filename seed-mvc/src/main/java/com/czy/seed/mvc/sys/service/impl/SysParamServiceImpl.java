44745f46c014aaa9e41621c61044e989442335b2
015349 <zhangyanhao@inner.czy.com>
2017-10-12 15:32:53 +0800

    Merge branch 'master' of http://10.131.0.171/003914/seed
    # Conflicts:
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/controller/SysDictController.java
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/entity/SysDict.java
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/entity/SysDictItem.java
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/mapper/SysDictMapper.java
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/mapper/SysParamsMapper.java
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/mapper/xml/SysDictMapper.xml
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/service/SysDictService.java
    #	seed-mvc/src/main/java/com/czy/seed/mvc/sys/service/impl/SysDictServiceImpl.java
    #	seed-mvc/src/main/resources/public/sys/dict/dict-group-edit.html
    #	seed-mvc/src/main/resources/public/sys/dict/dict-group-edit.js
    #	seed-mvc/src/main/resources/public/sys/dict/dict-group.html
    #	seed-mvc/src/main/resources/public/sys/dict/dict-group.js
    #	seed-mvc/src/main/resources/public/sys/dict/dict-index.html
    #	seed-mvc/src/main/resources/public/sys/dict/dict-index.js
    #	seed-mvc/src/main/resources/public/sys/dict/dict-item-edit.html
    #	seed-mvc/src/main/resources/public/sys/dict/dict-item-edit.js
    #	seed-mvc/src/main/resources/public/sys/dict/dict-item.html
    #	seed-mvc/src/main/resources/public/sys/dict/dict-item.js

--- seed-mvc/src/main/java/com/czy/seed/mvc/sys/service/impl/SysParamServiceImpl.java
+++ seed-mvc/src/main/java/com/czy/seed/mvc/sys/service/impl/SysParamServiceImpl.java
@@ -2,7 +2,7 @@ package com.czy.seed.mvc.sys.service.impl;
 
 import com.czy.seed.mvc.base.service.impl.BaseServiceImpl;
 import com.czy.seed.mvc.sys.entity.SysParam;
-import com.czy.seed.mvc.sys.mapper.SysParamsMapper;
+import com.czy.seed.mvc.sys.mapper.SysParamMapper;
 import com.czy.seed.mvc.sys.service.SysParamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SysParamServiceImpl extends BaseServiceImpl<SysParam> implements SysParamService {
     @Autowired
-    SysParamsMapper sysParamsMapper;
+    SysParamMapper sysParamsMapper;
 
     @Override
     public int updateActive(Long id) {
