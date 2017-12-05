84f8d0a1b50ed7ac88e917ee4b759e2e983acd7a
panlc <panlc2004@126.com>
2017-12-05 17:16:21 +0800

    验证码登陆功能完善

--- seed-mvc/src/main/java/com/czy/seed/mvc/auth/AuthController.java
+++ seed-mvc/src/main/java/com/czy/seed/mvc/auth/AuthController.java
@@ -18,8 +18,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class AuthController {
@@ -49,8 +47,7 @@ public class AuthController {
     @RequestMapping(value = AuthConstants.LOGIN_AJAX_URL, method = RequestMethod.GET)
     @ResponseBody
     public Res loginAjax() {
-        Map<String, Object> result = new HashMap<String, Object>();
-        return Res.custom(401, "you need login!", null);
+        return Res.custom(401, "请先登陆系统", null);
     }
 
     /**
@@ -73,27 +70,28 @@ public class AuthController {
      * 校验验证码
      *
      * @param verifyCode
-     * @param request
+     * @param cachedVerifyCode
+     * @param verifyCodeGenTime
      * @return
      */
-    private boolean captchaIsRight(String verifyCode, HttpServletRequest request) {
-        boolean res = true;
+    private CaptchaValidateInfo captchaIsRight(String verifyCode, String cachedVerifyCode, Long verifyCodeGenTime) {
+        CaptchaValidateInfo res = new CaptchaValidateInfo(true, "");
+        String error = "验证码错误";
+        String timeout = "验证码过期";
         //校验验证码
         if (NullUtil.isEmpty(verifyCode)) {
-            res = false;
+            res = new CaptchaValidateInfo(false, error);
         }
-        HttpSession session = request.getSession();
         //校验内容是否正确
-        if (!verifyCode.equalsIgnoreCase(session.getAttribute(AuthConstants.VERIFY_CODE).toString())) {
-            res = false;
+        else if (!verifyCode.equalsIgnoreCase(cachedVerifyCode)) {
+            res = new CaptchaValidateInfo(false, error);
         } else {
             //校验时间是否过期：30秒
-            Long verifyCodeGenTime = (Long) session.getAttribute(AuthConstants.VERIFY_CODE_GEN_TIME);
             Long now = System.currentTimeMillis();
             //超时时间
             int checkLimit = 30000;
             if (now - verifyCodeGenTime > checkLimit) {
-                res = false;
+                res = new CaptchaValidateInfo(false, timeout);
             }
         }
         return res;
@@ -106,9 +104,12 @@ public class AuthController {
                                  @RequestParam(defaultValue = "") String verifyCode,
                                  HttpServletRequest request) {
         //校验验证码
-        boolean captcha = captchaIsRight(verifyCode, request);
-        if (!captcha) {
-            return Res.custom(401, "验证码错误", null);
+        HttpSession session = request.getSession();
+        String cachedVerifyCode = session.getAttribute(AuthConstants.VERIFY_CODE).toString();
+        Long verifyCodeGenTime = (Long) session.getAttribute(AuthConstants.VERIFY_CODE_GEN_TIME);
+        CaptchaValidateInfo captcha = captchaIsRight(verifyCode, cachedVerifyCode, verifyCodeGenTime);
+        if (!captcha.isValidate()) {
+            return Res.custom(401, captcha.getMsg(), null);
         }
 
         //校验账号、密码
@@ -123,13 +124,40 @@ public class AuthController {
         try {
             Authentication authentication = authenticationManager.authenticate(authRequest);
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            HttpSession session = request.getSession();
             // 这个非常重要，否则验证后将无法登陆
             session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
         } catch (AuthenticationException ex) {
             return Res.custom(401, "账号或密码错误", null);
         }
         return Res.ok();
+    }
+
+    /**
+     * 校验信息封装类
+     */
+    class CaptchaValidateInfo {
+
+        public CaptchaValidateInfo(boolean validate, String msg) {
+            this.validate = validate;
+            this.msg = msg;
+        }
+
+        /**
+         * 是否校验通过
+         */
+        private boolean validate;
+        /**
+         * 校验描述信息
+         */
+        private String msg;
+
+        public boolean isValidate() {
+            return validate;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
     }
 
 }
