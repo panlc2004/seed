0b4eae7866ef1d8f7f560c36ec37a9c62b0f404a
panlc <panlc2004@126.com>
2017-12-15 14:57:53 +0800

    inrich

--- demo/src/main/resources/public/title.js
+++ demo/src/main/resources/public/title.js
@@ -1,10 +1,10 @@
 // 定义应用网页title
-document.title = 'DEMO';
+document.title = 'DEMO2';
 //功能页加载方式
 window.pageloadInTab = false
 // 定义应用名称
 // window.ctx = "seed";
 //系统名称——完全
-window.appName_full = "czy seed"
+window.appName_full = "czy demo2"
 //系统名称——缩写
-window.appName_simple = "seed"
+window.appName_simple = "demo3"
